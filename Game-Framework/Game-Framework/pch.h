c37b703af7373412e08c4757468e58b4665a9621
John Loeffler <John.Loeffler@gmail.com>
2019-07-25 19:18:52 -0400

    Organized Files

---
+++ Game-Framework/Game-Framework/pch.h
@@ -0,0 +1,21 @@
+#ifndef PCH_H
+#define PCH_H
+
+#include "Headers/player.hpp"
+#include "Headers/game-piece.hpp"
+#include "Headers/game-framework.h"
+#include "Headers/game-framework-exception.h"
+#include "Headers/game-board.hpp"
+
+namespace GameFrame{
+  using namespace std;
+  class Player;
+  class Players;
+  class GamePiece;
+  class GamePieces;
+  class GameState;
+  class GameBoard;
+  class GameController;
+}
+
+#endif //PCH_H
