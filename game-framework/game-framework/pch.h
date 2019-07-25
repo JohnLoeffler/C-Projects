6a79a05642b728098d8d0750850d912b37292d16
John Loeffler <John.Loeffler@gmail.com>
2019-07-25 18:40:46 -0400

    The whole thing is effed

--- game-framework/game-framework/pch.h
+++ game-framework/game-framework/pch.h
@@ -1,14 +1,23 @@
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
-
 #ifndef PCH_H
 #define PCH_H
 
-// TODO: add headers that you want to pre-compile here
+#include "../../14.16.27023/include/limits.h"
+#include "Headers/game-board.hpp"
+#include "Headers/game-controller.hpp"
+#include "Headers/game-framework-exception.hpp"
+#include "Headers/game-piece.hpp"
+#include "Headers/game-state.hpp"
+#include "Headers/player.hpp"
 
-#endif //PCH_H
+namespace GameFrame {
+using namespace std;
+class Player;
+class Players;
+class GamePiece;
+class GamePieces;
+class GameBoard;
+class GameState;
+class GameController;
+}  // namespace GameFrame
+
+#endif  // PCH_H
