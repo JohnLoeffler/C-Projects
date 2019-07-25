ab748e01fc9acf5caa5fc00277f6529a5e6778b7
John Loeffler <John.Loeffler@gmail.com>
2019-07-25 02:58:45 -0400

    Added Player Base Class Header (Player.hpp)

--- GameFramework/Headers/Player.hpp
+++ GameFramework/Headers/Player.hpp
@@ -1,1 +1,18 @@
+#ifndef PLAYER_HPP
+#define PLAYER_HPP
 
+class GamePiece;
+
+class Player{
+  GamePiece*  GamePieces;
+  int         ID;
+public:
+  virtual Player();
+  virtual ~Player();
+
+  inline void       SetGamePieces(GamePiece* pieces){this->GamePieces = pieces;}
+  inline void       SetID(int id)                   {this->ID = id;}
+  inline GamePiece* GetGamePieces()                 {return this->GamePieces;}
+  inline int        GetID()                         {return this->ID;}
+};
+#endif // PLAYER_HPP
