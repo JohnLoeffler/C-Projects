3df6f2115c57b2da68c1aeeff2b8091ed90c6b4c
John Loeffler <John.Loeffler@gmail.com>
2019-07-25 03:13:51 -0400

    Updated "package.hpp" to include all headers; reformating "player.hpp"

--- GameFramework/Headers/Player.hpp
+++ GameFramework/Headers/Player.hpp
@@ -4,15 +4,17 @@
 class GamePiece;
 
 class Player{
-  GamePiece*  GamePieces;
-  int         ID;
+  GamePiece*        GamePieces;
+  int               ID;
 public:
-  virtual Player();
-  virtual ~Player();
+                    Player()  {this->GamePieces = nullptr; this->ID = INT_MIN;}
+  virtual           ~Player();
 
   inline void       SetGamePieces(GamePiece* pieces){this->GamePieces = pieces;}
   inline void       SetID(int id)                   {this->ID = id;}
   inline GamePiece* GetGamePieces()                 {return this->GamePieces;}
   inline int        GetID()                         {return this->ID;}
+
+  virtual void      Action() =0;
 };
 #endif // PLAYER_HPP
