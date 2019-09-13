da4a4d64975fd3efae62c34f2e6e69b7a4827ee3
John Loeffler <John.Loeffler@gmail.com>
2019-09-13 01:11:08 -0400

    Building Unit Tests

---
+++ Game-Framework/Game-Framework-Unit-Tests/Player-Unit-Test.cpp
@@ -0,0 +1,42 @@
+#include "CppUnitTest.h"
+#include "../Game-Framework/Headers/player.h"
+#include "stdafx.h"
+
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace GameFrameworkUnitTests {
+  TEST_CLASS(PlayerUnitTest){
+  public :
+
+    TEST_METHOD(TestMethod1){
+      // TODO: Your test code here
+    }
+
+    TEST_METHOD(TestMethod1){
+      // TODO: Your test code here
+    }
+
+    TEST_METHOD(TestMethod1){
+      // TODO: Your test code here
+    }
+  };
+
+  TEST_CLASS(Players){
+    public :
+
+    TEST_METHOD(TestMethod1){
+    // TODO: Your test code here
+    }
+
+    TEST_METHOD(TestMethod1){
+    // TODO: Your test code here
+    }
+
+    TEST_METHOD(TestMethod1){
+    // TODO: Your test code here
+    }
+  
+  };
+}
+
