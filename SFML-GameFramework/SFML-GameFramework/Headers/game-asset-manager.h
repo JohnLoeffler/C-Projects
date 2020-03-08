cd1be4ae57b6455461b832cd8f23aae22eaa7a47
John Loeffler <John.Loeffler@gmail.com>
2020-03-08 16:21:37 -0400

    Added the ReadInAssetManifest function and provided default implementations for all functions to clear up a linker error when extending class.

--- SFML-GameFramework/SFML-GameFramework/Headers/game-asset-manager.h
+++ SFML-GameFramework/SFML-GameFramework/Headers/game-asset-manager.h
@@ -1,40 +1,50 @@
 #ifndef GAMEASSETMANAGER_H
 #define GAMEASSETMANAGER_H
 #pragma once
+#include "../Headers/game-asset-type.h"
+
 
 namespace GameFramework{
   class GameAssetManager{
   private:
   public:
     
-    GameAssetManager();
+    GameAssetManager(){}
 
-    virtual ~GameAssetManager();
+    virtual ~GameAssetManager(){}
+  
+    /**
+    * \fn ReadInAssetManifest()
+    * \brief Reads in the file locations for all the game assets to be used
+    * \param path A string of the path to the file containing the locations of the necessary assets
+    * \return int Returns the number of files located, -1 if the manifest file cannot be found
+    */
+    virtual int ReadInAssetManifest(const char* path){return -1;}
 
     /**
     * \fn AddAsset()
     * \brief Adds a Asset to the GameAssetManager
     * \param path A string of the path to the file to be loaded
-    * \return true if asset was loaded from the indicated file, false otherwise.
+    * \return True if asset was successfully added, false otherwise
     */
-    virtual bool AddAsset(std::string path);
+    virtual bool AddAsset(GameAssetType type, std::string name, std::string path){return false;}
     
     /**
     * \fn EraseFile()
     * \brief Removes an asset from the asset map
     * \param index The index of the asset in the map
-    * \return If index is valid, returns true if the asset was erased, false otherwise.
+    * \return True if the asset was erased, false otherwise.
     * \throws GameFrameworkException If index is greater than or equal to the size of the asset map
     */
-    virtual bool EraseAsset(unsigned int index);
+    virtual bool EraseAsset(GameAssetType type, std::string name){ return false; }
 
     /**
     * \fn GetNumberOfAssets(AssetType)
     * \brief  Returns the size of the asset map for the given type
     * \param  The AssetType for the map you want to check
-    * \return an unsigned int of the size of an asset map
+    * \return An unsigned int of the size of an asset map
     */
-    virtual unsigned int GetNumberOfAssets();
+    virtual unsigned int GetNumberOfAssets(GameAssetType type){return 0;}
   };
 
 }
